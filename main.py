d51b3630f65a1fc03bd8c06e3c47f4de9121b23e
ClxnZkx <134143661+ClxnZkx@users.noreply.github.com>
2023-11-26 14:33:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+nom = print
+nom = input ("Quel et ton nom ? ")
+print()
+print(nom)
+print()
+print("Bonjour,")
+print()
+print("Vous vous appelez  " + nom + ".")
+
+age = print
+print()
+age = input ("Quel age a tu ?")
+print()
+print ("Vous avez " + age + "ans")
+print(type(nom))
+print(int=age)
+print (str(nom))
+print("")
+input (str("Merci pour votre confiance a bientot"))
